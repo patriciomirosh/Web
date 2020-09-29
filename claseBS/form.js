3b912b7b128fd6b07ee7d7abc5afe6dc4a27d4e0
patriciomirosh <71236425+patriciomirosh@users.noreply.github.com>
2020-09-29 14:32:45 -0300

    Add files via upload

---
+++ claseBS/form.js
@@ -0,0 +1,7 @@
+function validate(){
+    var nombre= document.getElementById("nombre");
+    var gasto =document.getElementById("gasto");
+    var gasto1=gasto*1000;
+    return alert("Hola ",nombre, "tu gasto equivale a :" ,gasto1,"lt/mes deberiamos empezar a ahorrar visita la pagina ahorros para saber como");
+
+}
